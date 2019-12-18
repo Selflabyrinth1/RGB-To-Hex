0445286fd4b5d03a554d6b3a79eb5b599e53c98b
Nicolas Paraksevas <nicolasparaskevas@hotmail.com>
2019-12-19 00:05:05 +0200

    Added progress bars

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function numtohex(c){
-  hex = c.toString(16);
+  let hex = c.toString(16);
   
   if (hex.length == 1) {
     hex = "0" + hex;
@@ -10,15 +10,29 @@ function numtohex(c){
 }
 
 function rgbtohex() {
-	red = +document.getElementById('red').value;
-	green = +document.getElementById('green').value;
-	blue = +document.getElementById('blue').value;
-	
+	let red = +document.getElementById('red').value;
+	let green = +document.getElementById('green').value;
+	let blue = +document.getElementById('blue').value;
 	
 	if (red > 255 || green > 255 || blue > 255 || red < 0 || green < 0 || blue < 0) {
 		document.getElementById('result').innerHTML = "Please enter a number between 0 and 255";
 	} else {
 		document.getElementById('result').innerHTML = "#" + numtohex(red) + numtohex(green) + numtohex(blue);
 	}
+	moveProgressBars(red,green,blue);
+}
+
+function moveProgressBars(redlimit, greenlimit, bluelimit) {
+	redlimit = (redlimit/255) * 100;
+	greenlimit = (greenlimit/255) * 100;
+	bluelimit = (bluelimit/255) * 100;
+
+	let elemRed = document.getElementById("redBar");
+	let elemGreen = document.getElementById("greenBar");
+	let elemBlue = document.getElementById("blueBar");
+	
+	elemRed.style.width = redlimit + "%";
+	elemGreen.style.width = greenlimit + "%";
+	elemBlue.style.width = bluelimit + "%";
 }
 
